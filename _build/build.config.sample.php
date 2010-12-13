2774131f24d0810f513ad83fc40c064699a10554
Romain <romain@meltingmedia.net>
2010-12-13 21:17:46 +0100

    config.php added

---
+++ _build/build.config.sample.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MODx Boilerplate
+ *
+ * Copyright 2010 by Romain Tripault // Melting Media <romain@melting-media.com>
+ *
+ * @package modxboilerplate
+ */
+/**
+ * Define the MODX path constants necessary for installation
+ *
+ * @package modxboilerplate
+ * @subpackage build
+ */
+define('MODX_BASE_PATH', dirname(dirname(__FILE__)).'/');
+define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
+define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
+define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
+define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
+
+
+define('MODX_BASE_URL','/');
+define('MODX_CORE_URL', MODX_BASE_URL . 'core/');
+define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
+define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
+define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
